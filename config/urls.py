2718b1cccb2cd9acbb359ee8569abb72804e93ae
rasty <hvozdetskiyyrii@gmail.com>
2024-06-09 20:22:45 +0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -1,26 +1,22 @@
 """
-URL configuration for config project.
+корневий urls
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+підключаєм urls з інших файлів конфігурації: core...
+
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core import views
+"""
+include - ф-ція підключає на загальний(config) url маршрут інші приложенія
 
+namespace - записуєм до якого приложенія відносяться urls адреса коли ми звертаємось до них в шаблонах:
+<a href="{% url "core:index" %}">Home</a> - це в base.html
+
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # '' === mysite.com
-    path('about/', views.about, name='about'),  # about/ - адреса сторінки
+
+    path('', include('core.urls', namespace='core'))
 ]
---
+++ core/urls.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+from django.urls import path
+from core import views
+
+app_name = 'main'  # назва приложенія(використовується в навігації в templates)
+
+urlpatterns = [
+    path('', views.index, name='index'),  # '' === mysite.com
+    path('about/', views.about, name='about'),  # about/ - адреса сторінки
+]
